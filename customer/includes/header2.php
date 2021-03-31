bdd729379e7b7fea9539bc35812059ca08ab74da
magicmayk <71372793+magicmayk@users.noreply.github.com>
2021-03-31 11:52:44 +0800

    Initial Commit

---
+++ customer/includes/header2.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CRoboto" rel="stylesheet">
+  <meta http-equiv="x-ua-compatible" content="IE=edge, chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="shortcut icon" href="//cdn.shopify.com/s/files/1/2484/9148/files/SDQSDSQ_32x32.png?v=1511436147" type="image/png">
+  <title>Premium Gold Co.</title>
+  <link href="styles/bootstrap.min.css" rel="stylesheet">
+  <link href="styles/backend.css" rel="stylesheet">
+  <link href="styles/style.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+  <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+
+  <div id="myCarouselg2" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarouselg2" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarouselg2" data-slide-to="1"></li>
+      <li data-target="#myCarouselg2" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active ">
+        <img src="images/goldheader.png" class="img-responsive" >
+      </div>
+
+      <div class="item">
+        <img src="images/goldheader.png" class="img-responsive" >
+      </div>
+    
+      <div class="item">
+        <img src="images/goldheader.png" class="img-responsive" >
+      </div>
+
+    </div>
+    
+    <!-- Wrapper End -->
+
+    <!-- Left control -->
+    <a class="left carousel-control" href="#myCarouselg2" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+
+    <!-- Right control -->
+    <a class="right carousel-control" href="#myCarouselg2" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+
+
+
+    <!-- END OF CAROUSEL HEADER -->
---
+++ customer/pay_offline.php
@@ -0,0 +1,53 @@
+<center><!-- center Starts -->
+
+<h1> Pay OffLine Using Method  </h1>
+
+<p class="text-muted" >
+
+If you have any questions, please feel free to <a href="../contact.php" >contact us,</a> our customer service center is working for you 24/7.
+
+</p>
+
+</center><!-- center Ends -->
+
+<hr>
+
+
+<div class="table-responsive" ><!-- table-responsive Starts -->
+
+<table class="table table-bordered table-hover table-striped" ><!-- table table-bordered table-hover table-striped Starts -->
+
+<thead><!-- thead Starts -->
+
+<tr>
+
+<th> Bank Account Details </th>
+
+<th> Easy Paisa,UBL Omni,Mobi Cash Details: </th>
+
+<th> Western Union Details: </th>
+
+</tr>
+
+</thead><!-- thead Ends -->
+
+<tbody><!-- tbody Starts -->
+
+<tr>
+
+<td> Bank Name:Premium Gold Co. 6066528972	 </td>
+
+<td> NIC#001234567 Mobile No:03334566931, Name:Abraham Montenegro </td>
+
+<td> Full Name:Abraham Montenegro, Mobile No:09777573913, Name:Premium Gold Co., Country:Philippines, N.I.C No:001234567
+</td>
+
+
+</tr>
+
+</tbody><!-- tbody Ends -->
+
+
+</table><!-- table table-bordered table-hover table-striped Ends -->
+
+</div><!-- table-responsive Ends -->
